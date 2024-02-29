70e182090a63c61bf674cec8f8bfb1daf8b507a3
Thai Duong <thaiduongvl2001@gmail.com>
2024-02-29 10:49:13 +0700

    Init source

--- common/enum.js
+++ common/enum.js
@@ -1,5 +1,11 @@
 'use strict';
 
+exports.RouterType = Object.freeze({
+    AUTH_BY_TOKEN_ONLY: 'TOKEN', // token-based auth, no csrf
+    NO_CSRF: 'API', // token/session auth, no csrf
+    WITH_CSRF: 'CSRF', // token/session auth, with csrf
+});
+
 exports.DataResponse = Object.freeze([
     { errorCode: '0', message: 'Success' },
     { errorCode: '1', message: 'Invalid Session' },
