b41245d2da6723b3fdb37d9614cb171c237c3496
Thai Duong <thaiduongvl2001@gmail.com>
2024-02-29 10:53:11 +0700

    Init source

---
+++ src/sql/role.js
@@ -0,0 +1,38 @@
+'use strict';
+
+module.exports = (sequelize, DataTypes) => {
+    const role = sequelize.define('role',
+        {
+            id: {
+                primaryKey: true,
+                autoIncrement: true,
+                type: DataTypes.INTEGER,
+                unique: true,
+            },
+            role_code: {
+                type: DataTypes.INTEGER,
+                allowNull: false,
+                unique: true,
+            },
+            role_name: {
+                type: DataTypes.STRING(255),
+                allowNull: false,
+            },
+        },
+        {
+            scopes: {
+                primaryKey: (id) => {
+                    return {
+                        where: {
+                            id: id,
+                        },
+                    };
+                },
+            },
+            tableName: 'role',
+        },
+    );
+
+    return role;
+};
+
